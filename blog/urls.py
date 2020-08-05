e62f20247ba1885c2db8bfe0d6f0c03148be10d9
dipakgithub-gvp <pawardipak482@gmail.com>
2020-08-05 22:16:16 +0530

    completed the personal portfolio project.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.blog_index, name="blog_index"),
+    path('<int:blog_id>',views.blog_post, name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Blog
 
 # Create your views here.
 def blog_index(request):
     blogs = Blog.objects.order_by('-date')
     return render(request,'blog/blog_index.html',{"blogs":blogs})
+
+def blog_post(request,blog_id):
+    # posts = Blog.objects.filter(sr_no=sr_no)
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, "blog/blog_post.html",{"blog":blog})   
